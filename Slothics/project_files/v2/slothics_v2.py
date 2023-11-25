d1851e12afcbf910f2bc095de84dea77a0b15761
Pittersss <pedromalaquias608@gmail.com>
2023-11-25 13:22:56 -0300

    Versão 2 completa

--- Slothics/project_files/v2/slothics_v2.py
+++ Slothics/project_files/v2/slothics_v2.py
@@ -1,19 +1,17 @@
 import PySimpleGUI as sg
 import button
-from playsound import playsound
+import pygame
 import os
 
 sg.theme('Black')
-music_title = ""
 musics = []
 for _, _, arquivo in os.walk('musics/'):
     musics = arquivo
-print(musics)
 
 
 layout = [
     [sg.Push(),sg.Image('images/capa_album.png'),sg.Push()],
-    [sg.Text(music_title), sg.Push()],
+    [sg.Text(musics[0], key="-TEXT-"), sg.Push()],
     [sg.Push(),sg.Button('', image_data=button.button_left_next, key='-PREVIOUS-', button_color=(sg.theme_background_color(), sg.theme_background_color()), border_width=0),
     sg.Button('', image_data=button.button_play, key='-PLAY-', button_color=(sg.theme_background_color(), sg.theme_background_color()),border_width=0),
     sg.Button('', image_data=button.button_rigth_next, key='-NEXT-', button_color=(sg.theme_background_color(), sg.theme_background_color()), border_width=0), sg.Push()]
@@ -25,6 +23,7 @@ window = sg.Window('Slothics', layout)
 # percorrer pela pasta
 # extrair nome dos arquivos
 i = 0
+pygame.mixer.init()
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED :
@@ -32,13 +31,30 @@ while True:
     if event == '-NEXT-':
         if i == (len(musics)-1):
             i = 0
-        else: i += 1
+            window.Element("-TEXT-").update(f'{musics[i]}')
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load(f'musics/{musics[i]}')
+            pygame.mixer.music.play(-1)
+        else:
+            i += 1
+            window.Element("-TEXT-").update(f'{musics[i]}')
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load(f'musics/{musics[i]}')
+            pygame.mixer.music.play(-1)
     if event == '-PREVIOUS-':
         if i == 0:
-            i = len(musics)-1
+            i = len(musics)-1          
+            window.Element("-TEXT-").update(f'{musics[i]}')
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load(f'musics/{musics[i]}')
+            pygame.mixer.music.play(-1)
         else:
             i -= 1
+            window.Element("-TEXT-").update(f'{musics[i]}')
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load(f'musics/{musics[i]}')
+            pygame.mixer.music.play(-1)
     if event == '-PLAY-':
-        playsound(f"musics/{musics[i]}", False)
-
+        pygame.mixer.music.load(f'musics/{musics[i]}')
+        pygame.mixer.music.play(-1)
 window.close()
