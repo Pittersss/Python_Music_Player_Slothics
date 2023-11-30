8669edd8870be289cf8fb533d8a90e7136aec20a
Pedro Malaquias <pedromalaquias608@gmail.com>
2023-11-30 14:52:04 -0300

    Mudanças
    Mais atualizações

--- Slothics/project_files/v3/slothics_v2.py
+++ Slothics/project_files/v3/slothics_v2.py
@@ -7,17 +7,41 @@ sg.theme('Black')
 musics = []
 for _, _, arquivo in os.walk('musics/'):
     musics = arquivo
+def extract_autor(music_path_name):
+    autor_name = ""
+    for i in range(len(music_path_name)):
+        if music_path_name[i] != " ":
+            autor_name += music_path_name[i]
+        else: break
+    return autor_name
+print(extract_autor("Matuê - Anos Luz.mp3"))
+def extract_music_title(music_path_name):
+    music_name = ""
+    def extract_initial_index():
+        for i in range(len(music_path_name)):
+            if music_path_name[i] == "-":
+                return i + 2
+    for j in range(extract_initial_index(), len(music_path_name)):
+        if music_path_name[j] != "-":
+            if music_path_name[j] == ".":
+                break
+            music_name += music_path_name[j]
+    return music_name
+print(extract_music_title("Aqua - Barbie Girl.mp3"))
+
+        
 
 
 layout = [
-    [sg.Push(),sg.Image('images/capa_album.png'),sg.Push()],
-    [sg.Text(musics[0][:-4], key="-TEXT-"), sg.Push()],
+    [sg.Image('images/capa_album.png'),sg.Push()],
+    [sg.Text(extract_music_title(musics[0]), key="-TITLE-"), sg.Push()],
+    [sg.Text(extract_autor(musics[0]),key="-AUTOR-"), sg.Push()],
     [sg.Push(),sg.Button('', image_data=button.button_left_next, key='-PREVIOUS-', button_color=(sg.theme_background_color(), sg.theme_background_color()), border_width=0),
     sg.Button('', image_data=button.button_pause, key='-PLAY-', button_color=(sg.theme_background_color(), sg.theme_background_color()),border_width=0),
     sg.Button('', image_data=button.button_rigth_next, key='-NEXT-', button_color=(sg.theme_background_color(), sg.theme_background_color()), border_width=0), sg.Push()]
     ]
 
-window = sg.Window('Slothics', layout)
+window = sg.Window('Slothics', layout,size=(320, 550))
 
 #os.path.getsize() -- > tamanho da pasta
 # percorrer pela pasta
@@ -32,26 +56,30 @@ while True:
     if event == '-NEXT-':
         if i == (len(musics)-1):
             i = 0
-            window.Element("-TEXT-").update(f'{musics[i][:-4]}')
+            window.Element("-TITLE-").update(extract_music_title(musics[i]))
+            window.Element("-AUTOR-").update(extract_autor(musics[i]))
             pygame.mixer.music.stop()
             pygame.mixer.music.load(f'musics/{musics[i]}')
             pygame.mixer.music.play(-1)
         else:
             i += 1
-            window.Element("-TEXT-").update(f'{musics[i][:-4]}')
+            window.Element("-TITLE-").update(extract_music_title(musics[i]))
+            window.Element("-AUTOR-").update(extract_autor(musics[i]))
             pygame.mixer.music.stop()
             pygame.mixer.music.load(f'musics/{musics[i]}')
             pygame.mixer.music.play(-1)
     if event == '-PREVIOUS-':
         if i == 0:
             i = len(musics)-1          
-            window.Element("-TEXT-").update(f'{musics[i][:-4]}')
+            window.Element("-TITLE-").update(extract_music_title(musics[i]))
+            window.Element("-AUTOR-").update(extract_autor(musics[i]))
             pygame.mixer.music.stop()
             pygame.mixer.music.load(f'musics/{musics[i]}')
             pygame.mixer.music.play(-1)
         else:
             i -= 1
-            window.Element("-TEXT-").update(f'{musics[i][:-4]}')
+            window.Element("-TITLE-").update(extract_music_title(musics[i]))
+            window.Element("-AUTOR-").update(extract_autor(musics[i]))
             pygame.mixer.music.stop()
             pygame.mixer.music.load(f'musics/{musics[i]}')
             pygame.mixer.music.play(-1)
