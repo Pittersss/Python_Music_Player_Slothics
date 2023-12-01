180c4686cd71d833fd9d013ea5a3ba7055d4fb00
Pedro Malaquias <pedromalaquias608@gmail.com>
2023-12-01 15:51:16 -0300

    Mudanças Finais

--- Slothics/project_files/v3/slothics_v2.py
+++ Slothics/project_files/v3/slothics_v2.py
@@ -10,11 +10,12 @@ for _, _, arquivo in os.walk('musics/'):
 def extract_autor(music_path_name):
     autor_name = ""
     for i in range(len(music_path_name)):
-        if music_path_name[i] != " ":
-            autor_name += music_path_name[i]
-        else: break
+        if music_path_name[i] == "-":
+            for j in range(0, i-1):
+                autor_name += music_path_name[j]
+            break
     return autor_name
-print(extract_autor("Matuê - Anos Luz.mp3"))
+
 def extract_music_title(music_path_name):
     music_name = ""
     def extract_initial_index():
@@ -27,15 +28,11 @@ def extract_music_title(music_path_name):
                 break
             music_name += music_path_name[j]
     return music_name
-print(extract_music_title("Aqua - Barbie Girl.mp3"))
-
-        
 
 
 layout = [
     [sg.Image('images/capa_album.png'),sg.Push()],
-    [sg.Text(extract_music_title(musics[0]), key="-TITLE-"), sg.Push()],
-    [sg.Text(extract_autor(musics[0]),key="-AUTOR-"), sg.Push()],
+    [sg.Text(extract_music_title(musics[0])+f"\n{extract_autor(musics[0])}", key="-TITLE-", font="Negrito"), sg.Push()],
     [sg.Push(),sg.Button('', image_data=button.button_left_next, key='-PREVIOUS-', button_color=(sg.theme_background_color(), sg.theme_background_color()), border_width=0),
     sg.Button('', image_data=button.button_pause, key='-PLAY-', button_color=(sg.theme_background_color(), sg.theme_background_color()),border_width=0),
     sg.Button('', image_data=button.button_rigth_next, key='-NEXT-', button_color=(sg.theme_background_color(), sg.theme_background_color()), border_width=0), sg.Push()]
@@ -43,56 +40,60 @@ layout = [
 
 window = sg.Window('Slothics', layout,size=(320, 550))
 
-#os.path.getsize() -- > tamanho da pasta
-# percorrer pela pasta
-# extrair nome dos arquivos
+
 i = 0
-click_play, is_pause = False, False
+click_play, is_pause, is_move = False, False, False
 pygame.mixer.init()
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED :
         break
     if event == '-NEXT-':
+        is_move = True
         if i == (len(musics)-1):
             i = 0
-            window.Element("-TITLE-").update(extract_music_title(musics[i]))
-            window.Element("-AUTOR-").update(extract_autor(musics[i]))
+            window.Element("-TITLE-").update(extract_music_title(musics[i])+f"\n{extract_autor(musics[i])}")
             pygame.mixer.music.stop()
             pygame.mixer.music.load(f'musics/{musics[i]}')
-            pygame.mixer.music.play(-1)
+            if click_play:
+                pygame.mixer.music.play(-1)
         else:
             i += 1
-            window.Element("-TITLE-").update(extract_music_title(musics[i]))
-            window.Element("-AUTOR-").update(extract_autor(musics[i]))
+            window.Element("-TITLE-").update(extract_music_title(musics[i])+f"\n{extract_autor(musics[i])}")
             pygame.mixer.music.stop()
             pygame.mixer.music.load(f'musics/{musics[i]}')
-            pygame.mixer.music.play(-1)
+            if click_play:
+                pygame.mixer.music.play(-1)
     if event == '-PREVIOUS-':
+        is_move = True
         if i == 0:
             i = len(musics)-1          
-            window.Element("-TITLE-").update(extract_music_title(musics[i]))
-            window.Element("-AUTOR-").update(extract_autor(musics[i]))
+            window.Element("-TITLE-").update(extract_music_title(musics[i])+f"\n{extract_autor(musics[i])}")
             pygame.mixer.music.stop()
             pygame.mixer.music.load(f'musics/{musics[i]}')
-            pygame.mixer.music.play(-1)
+            if click_play:
+                pygame.mixer.music.play(-1)
         else:
             i -= 1
-            window.Element("-TITLE-").update(extract_music_title(musics[i]))
-            window.Element("-AUTOR-").update(extract_autor(musics[i]))
+            window.Element("-TITLE-").update(extract_music_title(musics[i])+f"\n{extract_autor(musics[i])}")
             pygame.mixer.music.stop()
             pygame.mixer.music.load(f'musics/{musics[i]}')
-            pygame.mixer.music.play(-1)
+            if click_play:
+                pygame.mixer.music.play(-1)
     if event == '-PLAY-':
         if not click_play:
-            window.Element("-PLAY-").Update(image_data=button.button_play)
+            window.Element("-PLAY-").update(image_data=button.button_play)
             click_play = True
             if not is_pause:
                 pygame.mixer.music.load(f'musics/{musics[i]}')
                 pygame.mixer.music.play(-1)
             else:
-                pygame.mixer.music.unpause()
-                is_pause = False
+                if is_move:
+                    pygame.mixer.music.play(-1)
+                    is_move = False
+                else:
+                    pygame.mixer.music.unpause()
+                    is_pause = False
         else:
             window.Element("-PLAY-").Update(image_data=button.button_pause)
             click_play = False
